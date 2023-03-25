b55623636112f0e0321d15d462cb164b9dfbcc57
Dark Two <115444279+0xDarkTwo@users.noreply.github.com>
2023-03-24 21:35:29 -0400

    fixed arguments and updated to Go 1.20

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module github.com/0xdarktwo/ghost-aio
 
-go 1.19
+go 1.20
 
 require github.com/joho/godotenv v1.5.1
--- internal/startup/startup.go
+++ internal/startup/startup.go
@@ -7,11 +7,11 @@ import (
 )
 
 func StartApp(id string, steamdir string, telnetport string) {
-	cmd := exec.Command(steamdir, "-applaunch "+id, "-windowed", "-novid", "-nojoy", "-noborder", "-w 960", "-h 540", "-x 0", "-y 0", "-refresh 30", "-netconport "+telnetport)
+	cmd := exec.Command(steamdir, "-applaunch", id, "-windowed", "-novid", "-nojoy", "-noborder", "-w", "960", "-h", "540", "-x", "0", "-y", "0", "-refresh", "30", "-netconport", telnetport)
 	err := cmd.Run()
 	if err != nil {
 		log.Fatal("Error starting app (Steam Not in Default Directory?)")
 	}
 	conn := telnet.InitConnection("localhost:" + telnetport)
-
+	log.Println(conn)
 }
